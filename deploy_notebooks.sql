85c84f689e48b61376cf2d624a0698ae28a1ca2b
ncarlson-mhnchicago <ncarlson@mhnchicago.org>
2025-06-30 13:31:24 -0500

    Update deploy_notebooks.sql

--- deploy_notebooks.sql
+++ deploy_notebooks.sql
@@ -8,6 +8,6 @@
 -- Create the Notebook
 CREATE OR REPLACE NOTEBOOK IDENTIFIER('"{{target_db}}"."{{target_schema}}"."{{notebook}}"')
     FROM '@"{{source_db}}"."{{source_schema}}"."{{source_repo}}"/branches/"{{source_branch}}"/{{notebook}}.ipynb'   
-    QUERY_WAREHOUSE = '{{source_wh}}'
+    QUERY_WAREHOUSE = '{{query_wh}}'
     MAIN_FILE = '{{notebook}}.ipynb';
 ALTER NOTEBOOK "{{target_db}}"."{{target_schema}}"."{{notebook}}" ADD LIVE VERSION FROM LAST;
